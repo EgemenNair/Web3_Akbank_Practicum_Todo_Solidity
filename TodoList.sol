318373ca8bc8bb23b5135decce1966f19a565840
Egemen Nair <108150376+EgemenNair@users.noreply.github.com>
2022-09-17 16:24:51 +0300

    Update TodoList.sol

--- TodoList.sol
+++ TodoList.sol
@@ -2,25 +2,31 @@
 pragma solidity 0.8.17;
 
 contract Todos {
+    // Initialize a struct for To Do's
     struct Todo {
         string text;
         bool completed;
     }
 
+    // Initialize a todos list consisting of Todo struct type items.
     Todo[] public todos;
 
+    // Create function to add a new to do item to todos list.
     function create(string calldata _text) public {
         todos.push(Todo(_text, false));
     }
 
+    // Get function to get the text and completed properties of Todo item in specified index:
     function get(uint _index) public view returns (string memory text, bool completed) {
         return (todos[_index].text, todos[_index].completed);
     }
 
+    // Function to update the text of Todo Struct item in specified index:
     function updateText(uint _index, string calldata _text) public {
         todos[_index].text = _text;
     }
 
+    // Function to toggle the completed property of Todo Struct item in specified index:
     function toggleCompleted(uint _index) public {
         todos[_index].completed = !todos[_index].completed;
     }
